baebfb1aebc22078c3795f6cf51986d4c273836f
tanishaasahni <73065453+tanishaasahni@users.noreply.github.com>
2021-02-16 14:08:19 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper{
+    constructor(x,radius){
+      var options = {
+          isStatic: false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2,
+      }
+      this.body=Bodies.circle(x,100,radius);
+      this.radius=radius;
+      World.add(world,this.body);
+
+    }
+    display(){
+      var pos =this.body.position;
+      ellipseMode(RADIUS);
+      push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(this.body.angle);
+        ellipse( 0, 0, this.radius, this.radius);
+        pop();
+    }
+}
